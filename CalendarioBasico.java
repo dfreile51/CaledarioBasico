28b0b5a05c1630426a17a4bf8f264b0f352aa1ce
Diego <dfreile51@gmail.com>
2021-11-22 14:09:47 +0100

    03 Añadido los tres metodos

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -5,7 +5,7 @@ public class CalendarioBasico {
     // El mes
     private int mes;
     // El año
-    private int año;
+    private int ano;
 
     /**
      * Crea un Calendario Básico
@@ -13,6 +13,38 @@ public class CalendarioBasico {
     public CalendarioBasico() {
         dia = 1;
         mes = 1;
-        año = 1;
+        ano = 1;
+    }
+    
+    public String obtenerFecha() {
+        String fecha = (dia + "-" + mes + "-" + ano);
+        return fecha;
+    }
+    
+    public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno) {
+        dia = nuevoDia;
+        mes = nuevoMes;
+        ano = nuevoAno;
+    }
+    
+    public void avanzarFecha() {
+        if (dia < 30) {
+            dia = dia + 1;
+        }
+        else {
+            dia = 1;
+            if (mes < 12) {
+                mes = mes + 1;
+            }
+            else {
+                mes = 1;
+                if (ano < 2099) {
+                    ano = ano + 1;
+                }
+                else {
+                    ano = 1;
+                }
+            }
+        }
     }
 }
