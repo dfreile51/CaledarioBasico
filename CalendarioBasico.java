2b4beeaab9b123b362098fc65a3a35bd445a3296
Diego <dfreile51@gmail.com>
2021-11-22 13:47:41 +0100

    02 Añadido las tres atributos

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -1,33 +1,18 @@
-
-/**
- * Write a description of class CalendarioBasico here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class CalendarioBasico
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class CalendarioBasico {
+    
+    // El dia
+    private int dia;
+    // El mes
+    private int mes;
+    // El año
+    private int año;
 
     /**
-     * Constructor for objects of class CalendarioBasico
+     * Crea un Calendario Básico
      */
-    public CalendarioBasico()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public CalendarioBasico() {
+        dia = 1;
+        mes = 1;
+        año = 1;
     }
 }
